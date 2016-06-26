9f1b0c01b26936383200a3dfd75623147dafa8ea
Jeff Bell <jeferb9@gmail.com>
2016-06-26 14:02:14 -0700

    adjust slick

--- js/script.js
+++ js/script.js
@@ -10,8 +10,10 @@ $('.autoplay').slick({
 	  slidesToShow: 1,
 	  slidesToScroll: 1,
 	  autoplay: true,
-	  autoplaySpeed: 7000,
+	  autoplaySpeed: 5000,
 	  fade: true,
 	  arrows: false,
 	  dots: true,
+	  pauseOnFocus: false,
+	  pauseOnHover: false
 });
